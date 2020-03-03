653e9dffb5b5fb0f5b17329f5fb23d82f7082c9d
Laura <laura_habdul@hotmail.fr>
2020-03-03 15:53:26 +0100

    Refactor ReadSymptomDataFromFile. Exceptions and stream handling

--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,6 +1,7 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,35 +14,38 @@ import java.util.List;
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
 	private String filepath;
-	
+
 	/**
-	 * 
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
 	 */
-	public ReadSymptomDataFromFile (String filepath) {
+	public ReadSymptomDataFromFile(String filepath) {
 		this.filepath = filepath;
 	}
-	
+
 	@Override
 	public List<String> GetSymptoms() {
-		ArrayList<String> result = new ArrayList<String>();
-		
+
+		ArrayList<String> symptomsList = new ArrayList<>();
+
 		if (filepath != null) {
-			try {
-				BufferedReader reader = new BufferedReader (new FileReader(filepath));
-				String line = reader.readLine();
-				
+			try (BufferedReader bufferedReader = new BufferedReader (new FileReader(filepath))) {
+				String line = bufferedReader.readLine();
 				while (line != null) {
-					result.add(line);
-					line = reader.readLine();
+					symptomsList.add(line);
+					line = bufferedReader.readLine();
 				}
-				reader.close();
+				for (String symptom : symptomsList) {
+					System.out.println("symptom from file: " + symptom);
+				}
+			} catch (FileNotFoundException e) {
+				System.err.println("Error while opening " + filepath);
+				e.printStackTrace();
 			} catch (IOException e) {
+				System.err.println("Error while reading " + filepath);
 				e.printStackTrace();
 			}
 		}
-		
-		return result;
-	}
 
+		return symptomsList;
+	}
 }
